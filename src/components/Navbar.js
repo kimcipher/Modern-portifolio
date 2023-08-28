41e4038c72e0945f02e0127ce5163df90b30163d
kimcipher <kimutaij192@gmail.com>
2023-08-28 14:48:59 +0300

    final

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,26 +1,42 @@
-import React from "react";
+import React, { useState } from "react";
 
 
 const Navbar = () => {
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setMenuOpen(!menuOpen);
+  };
+
+  const closeMenu = () => {
+    setMenuOpen(false);
+  };
+
   return (
     <nav className="navbar">
-      <ul className="navbar-list">
-        <li className="navbar-item">
-          <a href="/" className="navbar-link">Home</a>
-        </li>
-        <li className="navbar-item">
-          <a href="#about" className="navbar-link">About</a>
-        </li>
-        <li className="navbar-item">
-          <a href="#projects" className="navbar-link">Projects</a>
-        </li>
-        <li className="navbar-item">
-          <a href="#testimonials" className="navbar-link">Testimonials</a>
-        </li>
-        <li className="navbar-item">
-          <a href="#contact" className="navbar-link">Contact</a>
-        </li>
-      </ul>
+      <button className={`menu-toggle ${menuOpen ? "active" : ""}`} onClick={toggleMenu}>
+        Menu
+      </button>
+      <div className={`overlay ${menuOpen ? "active" : ""}`} onClick={closeMenu} />
+      <div className={`navbar-drawer ${menuOpen ? "open" : ""}`}>
+        <ul className="navbar-list">
+          <li className="navbar-item">
+            <a href="/" className="navbar-link">Home</a>
+          </li>
+          <li className="navbar-item">
+            <a href="#about" className="navbar-link">About</a>
+          </li>
+          <li className="navbar-item">
+            <a href="#projects" className="navbar-link">Projects</a>
+          </li>
+          <li className="navbar-item">
+            <a href="#testimonials" className="navbar-link">Testimonials</a>
+          </li>
+          <li className="navbar-item">
+            <a href="#contact" className="navbar-link">Contact</a>
+          </li>
+        </ul>
+      </div>
     </nav>
   );
 };
--- src/components/Testimonials.js
+++ src/components/Testimonials.js
@@ -17,11 +17,6 @@ const testimonials = [
     content: " Et harum quidem rerum facilis est et expedita distinctio. Nam libero tempore, cum soluta nobis est eligendi optio cumque nihil impedit quo minus id quod maxime placeat facere possimus, omnis voluptas assumenda est, omnis dolor repellendus. Temporibus autem quibusdam et aut officiis debitis aut rerum necessitatibus saepe eveniet ut et voluptates repudiandae sint et molestiae non recusandae.",
     imageSrc: "https://images.pexels.com/photos/3799786/pexels-photo-3799786.jpeg?auto=compress&cs=tinysrgb&w=600", 
   },
-  {
-    name: "Michael Smith",
-    content: "Nulla aliquet porttitor venenatis. Donec a dui et dui fringilla consectetur id nec massa. Aliquam erat volutpat. Sed ut dui ut lacus dictum fermentum vel tincidunt neque. Sed sed lacinia lectus.",
-    imageSrc: "https://images.pexels.com/photos/3799786/pexels-photo-3799786.jpeg?auto=compress&cs=tinysrgb&w=600", 
-  },
 
 ];
 
@@ -30,36 +25,25 @@ const Testimonials = () => {
     <section id="testimonials" className="testimonials-section">
       <div className="container mx-auto">
         <h2 className="testimonials-title">Testimonials</h2>
-        <div id="testimonialCarousel" className="carousel slide" data-bs-ride="carousel">
-          <div className="carousel-inner">
-            {testimonials.map((testimonial, index) => (
-              <div key={index} className={`carousel-item ${index === 0 ? "active" : ""}`}>
-                <div className="testimonial-card">
-                  <div className="testimonial-content">
-                    <p>{testimonial.content}</p>
-                  </div>
-                  <div className="testimonial-info">
-                    <img
-                      src={testimonial.imageSrc}
-                      alt={`Testimonial from ${testimonial.name}`}
-                    />
-                    <p className="testimonial-name">{testimonial.name}</p>
-                  </div>
-                </div>
+        <div className="testimonial-cards-container">
+          {testimonials.map((testimonial, index) => (
+            <div key={index} className="testimonial-card">
+              <div className="testimonial-content">
+                <p>{testimonial.content}</p>
               </div>
-            ))}
-          </div>
-          <button className="carousel-control-prev" type="button" data-bs-target="#testimonialCarousel" data-bs-slide="prev">
-            <span className="carousel-control-prev-icon" aria-hidden="true"></span>
-            <span className="visually-hidden">Previous</span>
-          </button>
-          <button className="carousel-control-next" type="button" data-bs-target="#testimonialCarousel" data-bs-slide="next">
-            <span className="carousel-control-next-icon" aria-hidden="true"></span>
-            <span className="visually-hidden">Next</span>
-          </button>
+              <div className="testimonial-info">
+                <img
+                  src={testimonial.imageSrc}
+                  alt={`Testimonial from ${testimonial.name}`}
+                />
+                <p className="testimonial-name">{testimonial.name}</p>
+              </div>
+            </div>
+          ))}
         </div>
       </div>
     </section>
   );
 };
+
 export default Testimonials;
